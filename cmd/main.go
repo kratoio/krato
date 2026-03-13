2216e3a674cd0dbd97df8dccb48047bde0ee24b2
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-03-13 09:17:31 +0500

    feat: add database connection and env example (#2)

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/kratoio/krato/internal/config"
+	"github.com/kratoio/krato/internal/database"
+)
+
 func main() {
+	cfg, err := config.Load()
+
+	if err != nil {
+		log.Fatalf("error loading config: %s", err)
+	}
+
+	dbpool, err := database.Connect(*cfg)
+
+	if err != nil {
+		log.Fatalf("error connecting to database: %s", err)
+	}
+
+	defer dbpool.Close()
+
+	if err := dbpool.Ping(context.Background()); err != nil {
+		log.Fatalf("error running sql query: %s", err)
+	}
 }
