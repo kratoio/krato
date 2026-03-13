455f1236b194f0744008154bb7c392f9a662fbfa
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-03-13 19:58:42 +0500

    feat: add HTTP server with Chi router (#5)

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 include .env
+
 DATABASE_URL := postgres://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST):$(DB_PORT)/$(DB_NAME)?sslmode=$(DB_SSL_MODE)
 
 migrate-up:
@@ -9,3 +10,6 @@ migrate-down:
 
 migrate-down-one:
 	migrate -path migrations -database $(DATABASE_URL) down 1
+
+run-server:
+	go run cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/kratoio/krato/internal/config"
 	"github.com/kratoio/krato/internal/database"
 )
@@ -26,4 +28,15 @@ func main() {
 	if err := dbpool.Ping(context.Background()); err != nil {
 		log.Fatalf("error running sql query: %s", err)
 	}
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	if err = http.ListenAndServe(":" + cfg.Port, r); err != nil {
+		log.Fatalf("error listening to the port %s: %s", cfg.Port, err)
+	}
+
 }
